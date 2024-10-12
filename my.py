f12780550ca5aa532b2d4694c47d3339709f83a7
Fantasy. <130067454+Stagnation47@users.noreply.github.com>
2024-10-12 16:45:46 +0800

    Add files via upload

---
+++ my.py
@@ -0,0 +1,22 @@
+#from jetson_inference import detectNet
+#from jetson_utils import videoSource, videoOutput
+import jetson.inference
+import jetson.utils
+
+#net = detectNet("ssd-mobilenet-v2", threshold=0.5)
+net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
+
+image_path = "/home/nvidia/jetson-inference/data/images/airplane_0.jpg"
+
+img = jetson.utils.loadImage(image_path)
+
+detections = net.Detect(img)
+
+if len(detections) >0:
+    detection = detections[0]
+    class_id = detections[0].ClassID
+    confidence = detections[0].Confidence
+    left,top,right,bottom = detections[0].Left, detections[0].Top, detections[0].Right, detections[0].Bottom 
+    print(detections[0])
+
+
